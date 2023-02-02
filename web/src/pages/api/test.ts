b0174d5258911e842b4d1aeb73547d53691051c5
Shrey Gupta <shreygupta97@gmail.com>
2023-02-01 19:34:45 -0500

    package name fixes

--- web/src/pages/api/test.ts
+++ web/src/pages/api/test.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "@prisma-monorepo-output-bug/prisma";
+import { prisma } from "@prisma-monorepo-esm-output-bug/package/dist";
 
 export default async (req: NextApiRequest, res: NextApiResponse) =>
   res.json(await prisma.user.findMany());
